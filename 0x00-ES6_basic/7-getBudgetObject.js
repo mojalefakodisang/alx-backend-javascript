fe13c90cc2689e3b9039222bdcc890a07f876178
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-08-09 20:13:12 +0200

    [Update] - Get Budget Object

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,12 @@
+/*  eslint-disable no-unused-vars */
+/*  eslint-disable */
+
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
